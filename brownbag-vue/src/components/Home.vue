2c3d68e2f6d416cbf0d3fd4a19e515bd6bb035ce
Daniel <Daniel@LAPTOP-IM14107M>
2020-03-03 09:38:23 +0800

    Vuejs Login First Draft

---
+++ brownbag-vue/src/components/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <header class="col-md-12">
+      <h1>Value Investing</h1>
+      <h2>Basics of Trading</h2>
+    </header>
+  </div>
+</template>
+
+<script>
+import UserService from '../service/user.service';
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      content: ''
+    };
+  },
+  mounted() {
+    UserService.getPublicContent().then(
+      response => {
+        this.content = response.data;
+      },
+      error => {
+        this.content =
+          (error.response && error.response.data) ||
+          error.message ||
+          error.toString();
+      }
+    );
+  }
+};
+</script>
+<style scoped>
+header {
+  display: block;
+}
+</style>
