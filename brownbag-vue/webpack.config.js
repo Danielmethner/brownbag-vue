abe2d34a18a8771f89ddcb05789060b347888172
OneCentral <danielmethnerek@gmail.com>
2020-06-02 18:24:53 +0800

    simplify webpack

--- brownbag-vue/webpack.config.js
+++ brownbag-vue/webpack.config.js
@@ -1,11 +1,5 @@
 const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
-  plugins: [new CompressionPlugin({
-      filename: '[path].gz[query]',
-      algorithm: 'gzip',
-      test: /\.js$|\.css$|\.html$/,
-      threshold: 1024,
-      minRatio: 0.8,
-    })],
+  plugins: [new CompressionPlugin()],
 };
